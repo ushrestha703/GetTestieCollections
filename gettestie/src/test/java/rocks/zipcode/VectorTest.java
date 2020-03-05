79c1f63e1bae4de39181613c77c65bcc66984bc4
Ujjwal Shrestha <ushrestha@zipcoders-MBP-5.localdomain>
2020-03-05 13:25:49 -0500

    Vector test done

--- gettestie/src/test/java/rocks/zipcode/VectorTest.java
+++ gettestie/src/test/java/rocks/zipcode/VectorTest.java
@@ -1,4 +1,56 @@
 package rocks.zipcode;
 
+import org.junit.Assert;
+import org.junit.Test;
+import java.util.Vector;
+
 public class VectorTest {
+    private Vector v = new Vector();
+    private Vector v_clone = new Vector();
+
+    @org.junit.Before
+    public void setUp() throws Exception { }
+
+    @org.junit.After
+    public void tearDown() throws Exception { }
+
+    @org.junit.Test
+    public void TestVectorAdd() {
+       v.add("Hydrogen");
+        Assert.assertTrue(v.contains("Hydrogen"));
+    }
+
+    @Test
+    public void TestVectorClear() {
+        v.clear();
+        Assert.assertTrue(v.isEmpty());
+    }
+
+    @Test
+    public void TestVectorClone(){
+        v_clone = (Vector)v.clone();
+        Assert.assertEquals(true, v.equals(v_clone));
+    }
+
+    @Test
+    public void TestVectorGet(){
+        Vector v = new Vector();
+        v.add("Hydrogen");
+        v.add("Helium");
+        v.add("Lithium");
+        v.add("Beryllium");
+        v.add("Boron");
+        Assert.assertEquals("Beryllium", v.get(3));
+    }
+    @Test
+    public void TestVectorLastIndex(){ // Returns the index of the last occurrence of the specified element in this vector, or -1 if this vector does not contain the element.
+        Vector v = new Vector();
+        v.add("Hydrogen");
+        v.add("Helium");
+        v.add("Lithium");
+        v.add("Beryllium");
+        v.add("Boron");
+        Assert.assertEquals(1, v.lastIndexOf("Helium"));
+    }
+
 }
